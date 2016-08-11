5a8509c7d439c3ed7a1f44aafa3e81f0ae1143f5
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2016-08-11 10:24:13 -0300

    build

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ if (config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
-var server = require('http').createServer(app);
+// var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: config.env !== 'production',
   path: '/socket.io-client'
@@ -32,12 +32,25 @@ require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
 
+var ParseServer = require('parse-server').ParseServer;
+var api = new ParseServer({
+  databaseURI: 'mongodb://ednilson:detoy955@ds153735.mlab.com:53735/meanshop', // Connection string for your MongoDB database
+  serverURL: 'http://localhost:1337/meanshop' // Don't forget to change to https if needed
+});
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', api);
+
+app.listen(1337, function() {
+  console.log('parse-server-example running on port 1337.');
+});
+
 // Start server
-function startServer() {
-  server.listen(config.port, config.ip, function() {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
-}
+// function startServer() {
+//   server.listen(config.port, config.ip, function() {
+//     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+//   });
+// }
 
 setImmediate(startServer);
 
